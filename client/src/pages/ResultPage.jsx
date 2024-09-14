3ab7a1a202f0d51b42bbf121353d2921c5415044
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-09-14 02:11:58 -0400

    Add tailwind to project

--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 export default function ResultPage() {
     const [endpoint, setEndpoint] = useState('');
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/github')  // Ensure the URL matches your Flask backend
+        fetch('/repos')
             .then(response => response.json())
-            .then(data => setEndpoint(data.endpoint))
-            .catch(error => console.error('Error fetching endpoint:', error));
+            .then(data => {
+                setEndpoint(data.endpoint);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching endpoint:', error);
+                setLoading(false);
+            });
     }, []);
 
     return (
         <div>
             <h1>Result Page</h1>
-            <p>Endpoint: {endpoint}</p>
+            {loading ? <p>Loading...</p> : <p>Endpoint: {endpoint}</p>}
         </div>
     );
 }
