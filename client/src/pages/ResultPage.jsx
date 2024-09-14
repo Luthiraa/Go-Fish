1f9f3d73d25c2a8934d8ece2749e83c19070b74f
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-09-14 12:16:25 -0400

    Refactor ResultPage and SearchPage components

--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -19,9 +19,15 @@ export default function ResultPage() {
     }, []);
 
     return (
-        <div>
-            <h1>Result Page</h1>
-            {loading ? <p>Loading...</p> : <p>Endpoint: {endpoint}</p>}
+        <div className="flex items-center justify-center min-h-screen p-8">
+            <div className="bg-white p-8 rounded shadow-md w-full max-w-6xl h-screen">
+                <div className='flex flex-col md:flex-row justify-between items-center'>
+                    <img src="logoFIN.png" alt="Logo" className="h-8 w-auto mb-4 md:hidden" />
+                    <h1 className="text-4xl font-bold mb-4">{new URLSearchParams(window.location.search).get('query')}</h1>
+                    <img src="logoFIN.png" alt="Logo" className="h-8 w-auto max-md:hidden" />
+                </div>
+                {loading ? <p>Loading...</p> : <p>Endpoint: {endpoint}</p>}
+            </div>
         </div>
     );
 }
