9124f9900f4dbe53a6c375a48632b74575775e27
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2024-09-15 04:24:05 -0400

    Refactor code to handle GitHub token and merge conflict

--- server/src/app.py
+++ server/src/app.py
@@ -21,43 +21,24 @@ def search_and_summarize():
     logging.debug(f"Received query: {query}")
     
     try:
-<<<<<<< HEAD
         summary, resources, image_url, reddit_embed, github_url, github_line_number, github_snippet, matching_products = process_search_and_summarize(query)
-=======
-        summary, resources, image_url, reddit_embed, snippet,line_number, file_url, matching_products = process_search_and_summarize(query)
-        # summary, resources, image_url, reddit_embed, github_results, matching_products = process_search_and_summarize(query)
->>>>>>> e975c5e15aaa9371e48efb7a2dfeb7dabee7be49
         important = extract_important_words(query)
         logging.debug(f"Summary: {summary}")
         logging.debug(f"Important: {important}")
         logging.debug(f"Resources: {resources}")
         logging.debug(f"Reddit Embed: {reddit_embed}")
-<<<<<<< HEAD
         logging.debug(f"Snippet: {github_snippet}")
         logging.debug(f"Line Number: {github_line_number}")
         logging.debug(f"File URL: {github_url}")
-=======
-        logging.debug(f"Snippet: {snippet}")
-        logging.debug(f"Line Number: {line_number}")
-        logging.debug(f"File URL: {file_url}")
-        # logging.debug(f"GH Results: {github_results}")
->>>>>>> e975c5e15aaa9371e48efb7a2dfeb7dabee7be49
         return jsonify({
             "summary": summary,
             "resources": resources,
             "important": important,
             "image_url": image_url,
             "reddit_embed": reddit_embed,
-<<<<<<< HEAD
             "snippet": github_snippet,
             "line_number": github_line_number,
             "file_url": github_url,
-=======
-            "snippet": snippet,
-            "line_number": line_number,
-            "file_url": file_url,
-            # "github_results": github_results,
->>>>>>> e975c5e15aaa9371e48efb7a2dfeb7dabee7be49
             "matching_products": matching_products
         })
     except Exception as e:
